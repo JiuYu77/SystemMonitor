b5af7d5ca82983005910def50aaddf97eccfb99b
JiuYu77 <1992817771@qq.com>
2024-08-07 21:36:04 +0800

    modify

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,31 @@
 #include <QApplication>
 #include <QCoreApplication>
 
+std::string getExecutablePath() {
+    char buf[PATH_MAX];
+    ssize_t len = readlink("/proc/self/exe", buf, sizeof(buf) - 1);
+    if (len < 0 || (size_t)len >= sizeof(buf)) {
+        throw std::runtime_error("Failed to readlink /proc/self/exe");
+    }
+    buf[len] = '\0';
+    return std::string(buf);
+}
+ 
 
 int main(int argc, char *argv[])
 {
+    std::string executablePath;
+    try {
+        executablePath = getExecutablePath();
+        std::cout << "Executable path: " << executablePath << std::endl;
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
+    QString parent_dir = SystemMonitorApp::getParentDir(QString::fromStdString(executablePath));
+    QString qt_plugins = parent_dir + QDir::separator() + "qt_plugins";
+    std::cout << "qt_plugins: " <<  qt_plugins.toStdString() << std::endl;
+    setenv("QT_PLUGIN_PATH", qt_plugins.toStdString().c_str(), 1);
+
     QApplication a(argc, argv);
     SystemMonitorApp w;
     w.show();
