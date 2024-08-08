8020b2c000fca9991cb694acd2f0c1cfb4005f5d
JiuYu77 <1992817771@qq.com>
2024-08-08 11:52:18 +0800

    modify

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include "systemmonitorapp.h"
 
 #include <QApplication>
-#include <QCoreApplication>
+#include <QTextCodec>
+
 
 // 获取可执行文件的绝对路径
 std::string getExecutablePath() {
@@ -24,12 +25,26 @@ int main(int argc, char *argv[])
     } catch (const std::runtime_error& e) {
         std::cerr << "Error: " << e.what() << std::endl;
     }
+
+
     QString parent_dir = SystemMonitorApp::getParentDir(QString::fromStdString(executablePath));
+    std::cout << "parent_dir: " <<  parent_dir.toStdString() << std::endl;
     QString qt_plugins = parent_dir + QDir::separator() + "qt_plugins";
+
     std::cout << "qt_plugins: " <<  qt_plugins.toStdString() << std::endl;
     setenv("QT_PLUGIN_PATH", qt_plugins.toStdString().c_str(), 1);
 
+
+    // size_t i = executablePath.find_last_of('/');
+    // std::string parent_dir = executablePath.substr(0, i);
+    // std::cout << "parent_dir: " <<  parent_dir << std::endl;
+    // std::string qt_plugins = parent_dir + QString(QDir::separator()).toStdString() + "qt_plugins";
+    // std::cout << "qt_plugins: " <<  qt_plugins << std::endl;
+    // setenv("QT_PLUGIN_PATH", qt_plugins.c_str(), 1);
+    std::cout << "setenv. " << std::endl;
+
     QApplication a(argc, argv);
+    std::cout << "app start. " << std::endl;
     SystemMonitorApp w;
     w.show();
     return a.exec();
